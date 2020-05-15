ce1aa42c1eba4ef519c3b0ffda45616d20f93c13
shizc <shizc@mail.open.com.cn>
2020-05-15 08:57:21 +0800

    注册中心Eureka、Consul简单使用

---
+++ client1-consul/src/main/java/org/chao/sc/client1consul/Client1ConsulApplication.java
@@ -0,0 +1,11 @@
+package org.chao.sc.client1consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client1ConsulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Client1ConsulApplication.class, args);
+    }
+}
